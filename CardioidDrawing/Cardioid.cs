0cd25583846bc972c538b93676f13b8e9940455a
Frederik Lundbeck Jørgensen <Frederiklundbeck@live.dk>
2017-03-03 11:44:08 +0100

    Add files via upload

---
+++ CardioidDrawing/Cardioid.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CardioidDrawing
+{
+    class Cardioid
+    {
+        public int PointsAmount { get; set; }
+        public int Times { get; set; }
+
+        public Color BackgroundColor { get; set; }
+        public Color ForegroundColor { get; set; }
+
+        PointF[] points;
+
+        public void AssignPoints()
+        {
+            float centerPointX = (float)Form1.Panel_Width / 2;
+            float centerPointY = (float)Form1.Panel_Height / 2;
+
+            double angle = 6.28319 / PointsAmount;
+            float distance = Form1.Panel_Width / 2;
+
+            points = new PointF[PointsAmount];
+
+            for (int i = 0; i < PointsAmount; i++)
+            {
+                float xPos = centerPointX + (float)Math.Cos(angle * i) * distance;
+                float yPos = centerPointY + (float)Math.Sin(angle * i) * distance;
+
+                points[i] = new PointF(xPos, yPos);
+            }
+        }
+
+        public void Draw(Graphics gfx)
+        {
+            AssignPoints();
+            gfx.FillRectangle(new SolidBrush(BackgroundColor), 0,0,Form1.Panel_Width,Form1.Panel_Height);
+            gfx.DrawEllipse(new Pen(new SolidBrush(ForegroundColor)), new RectangleF(0, 0, Form1.Panel_Width,Form1.Panel_Height));
+
+            for (int i = 0; i < PointsAmount; i++)
+            {
+                int too = i * Times;
+
+                while (too > PointsAmount-1)
+                {
+                    too -= PointsAmount;
+                }
+
+                gfx.DrawLine(new Pen(ForegroundColor), points[i], points[too]);
+            }
+        }
+    }
+}
---
+++ CardioidDrawing/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CardioidDrawing
+{
+    public partial class Form1 : Form
+    {
+        Cardioid cardioid = new Cardioid();
+
+        public static int Panel_Width;
+        public static int Panel_Height;
+
+        public Form1()
+        {
+            InitializeComponent();
+            Panel_Width = panel1.Width;
+            Panel_Height = panel1.Height;
+
+            cardioid.PointsAmount = 200;
+            cardioid.Times = 2;
+            cardioid.ForegroundColor = Color.Black;
+            cardioid.BackgroundColor = Color.White;
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            cardioid.PointsAmount = (int)numericUpDown1.Value;
+            cardioid.Times = (int)numericUpDown2.Value;
+            cardioid.Draw(e.Graphics);
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            panel1.Invalidate();
+        }
+
+        private void numericUpDown2_ValueChanged(object sender, EventArgs e)
+        {
+            panel1.Invalidate();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DialogResult result = colorDialog1.ShowDialog();
+
+            if (result == DialogResult.OK)
+            {
+                cardioid.ForegroundColor = colorDialog1.Color;
+                button1.BackColor = colorDialog1.Color;
+                panel1.Invalidate();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DialogResult result = colorDialog2.ShowDialog();
+
+            if (result == DialogResult.OK)
+            {
+                cardioid.BackgroundColor = colorDialog2.Color;
+                button2.BackColor = colorDialog2.Color;
+                panel1.Invalidate();
+            }
+        }
+    }
+}
